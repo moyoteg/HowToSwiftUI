d6fd66d17295e53496dced1d54fcb8fdf6174982
Moi Gutierrez <moyoteg@gmail.com>
2021-02-01 18:03:56 -0600

    Initial Commit

---
+++ HowToSwiftUI/HowToSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToSwiftUIApp.swift
+//  HowToSwiftUI
+//
+//  Created by Moi Gutierrez on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct HowToSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
