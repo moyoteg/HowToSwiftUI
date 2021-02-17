8795523c878b4191e800f3df646b4332362ab9ac
Moi Gutierrez <moyoteg@gmail.com>
2021-02-16 22:59:55 -0600

    updated example to use swiftUiComponents package

--- HowToSwiftUI/How to's/ShowPopUp.swift
+++ HowToSwiftUI/How to's/ShowPopUp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftUIComponents
 
 struct ShowPopUP: View {
     
@@ -31,54 +32,4 @@ struct ShowPopUP: View {
             }
         }
     }
-}
-
-struct PopUp<PopUpContent: View>: ViewModifier {
-    
-    @Binding private var isPresented: Bool
-    let popUpContent: () -> PopUpContent
-    let onDismiss: (() -> Void)?
-    
-    func body(content: Content) -> some View {
-        ZStack {
-            content
-            if isPresented {
-                popUpContent()
-                    .onDisappear {
-                        onDismiss?()
-                    }
-                    .padding()
-                    .background(
-                        Rectangle()
-                            .fill(Color.white)
-                            .cornerRadius(15)
-                        
-                        , alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .transition(.scale)
-                    .padding(.top, 42)
-                    .shadow(radius: 10)
-            }
-        }
-
-    }
-
-    
-    public init(isPresented: Binding<Bool>,
-                onDismiss: (() -> Void)? = nil,
-                @ViewBuilder popUpContent: @escaping () -> PopUpContent) {
-        self._isPresented = isPresented
-        self.onDismiss = onDismiss
-        self.popUpContent = popUpContent
-    }
-}
-
-extension View {
-    
-    public func popUp<Content>(isPresented: Binding<Bool>,
-                               onDismiss: (() -> Void)? = nil,
-                               @ViewBuilder content: @escaping () -> Content
-    ) -> some View where Content : View {
-        self.modifier(PopUp(isPresented: isPresented, onDismiss: onDismiss, popUpContent: content))
-    }
-
 }
