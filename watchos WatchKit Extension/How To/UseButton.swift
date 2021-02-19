dfa39bd4fde1ab554e4f33ee1b99c531053921bd
Moi Gutierrez <moyoteg@gmail.com>
2021-02-19 02:56:03 -0600

    updates

---
+++ watchos WatchKit Extension/How To/UseButton.swift
@@ -0,0 +1,32 @@
+//
+//  UseButton.swift
+//  watchos WatchKit Extension
+//
+//  Created by Moi Gutierrez on 2/19/21.
+//
+
+import SwiftUI
+
+struct UseButton: View {
+    
+    @State var toggle = false
+    
+    var body: some View {
+        Button("toggle") {
+            withAnimation {
+                toggle.toggle()
+            }
+        }
+        .padding()
+        if toggle {
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: 40, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .cornerRadius(5)
+                .shadow(color: Color.green, radius: 3)
+                .padding()
+                .transition(.scale)
+        }
+    }
+    
+}
