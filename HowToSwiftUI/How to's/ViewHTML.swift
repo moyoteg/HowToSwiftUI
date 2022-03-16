cc4fa41704f8dea636977c9b9f11f8b35eb79215
Moi Gutierrez <moyoteg@gmail.com>
2022-03-15 20:14:50 -0600

    finished ViewHTML example

--- HowToSwiftUI/How to's/ViewHTML.swift
+++ HowToSwiftUI/How to's/ViewHTML.swift
@@ -7,9 +7,23 @@
 
 import SwiftUI
 
+import SwiftUIComponents
+
 struct ViewHTML: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HTMLView(htmlText:
+                        .constant(
+                    """
+                    <html>
+                    <body>
+                    <h1>
+                    Hello World
+                    </h1>
+                    </body>
+                    </html>
+                    """
+                        )
+        )
     }
 }
 
