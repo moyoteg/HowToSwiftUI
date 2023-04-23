047e3e13700ce1bd0dfa8427f14105d391623890
Moi Gutierrez <moyoteg@gmail.com>
2023-04-23 17:31:03 -0500

    updated ChatThread

--- HowToSwiftUI/How to's/UseAIChatGPT.swift
+++ HowToSwiftUI/How to's/UseAIChatGPT.swift
@@ -42,28 +42,29 @@ struct ChatThread: View {
     @State private var showAlert = false
     @State private var alertTitle = ""
     @State private var alertMessage = ""
-    @State private var apiKey = "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"
+    @State private var apiKey = "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"
     //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
-    
+    //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
+
     @State var CHATGPTtemperature = 0.0
 
 var body: some View {
-    
+
     Stepper("temperature \(CHATGPTtemperature, specifier: "%.1f")", value: $CHATGPTtemperature, in: 0.0...1.0, step: 0.1)
         .padding()
         .shadow(radius: 5)
-    
+
 //    Divider()
-    
+
     TextField("Enter Chat GPT apiKey", text: $apiKey)
         .padding()
         .minimumScaleFactor(0.3)
 //        .lineLimit(3)
         .shadow(radius: 5)
         .clearButton(text: $apiKey)
-    
+
 //    Divider()
-    
+
     VStack {
         List {
             ForEach(messages) { message in
@@ -79,13 +80,13 @@ var body: some View {
                 }
             }
         }
-        
+
         HStack {
             TextField("Enter message", text: $newMessage)
                 .clearButton(text: $newMessage)
                 .minimumScaleFactor(0.3)
                 .lineLimit(3)
-            
+
             Button(action: {
                 self.sendMessage()
             }) {
@@ -108,11 +109,11 @@ func sendMessage() {
         self.showAlert = true
         return
     }
-    
+
     let message = Message(content: newMessage, sender: "Me")
     messages.append(message)
     newMessage = ""
-    
+
     // Send message to chatgpt API
     let parameters:[String : Any] = [
         "prompt": message.content,
@@ -125,7 +126,7 @@ func sendMessage() {
     request.httpMethod = "POST"
     request.allHTTPHeaderFields = headers
     request.httpBody = try! JSONSerialization.data(withJSONObject: parameters, options: [])
-    
+
     let session = URLSession.shared
     let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
         if (error != nil) {
@@ -135,13 +136,16 @@ func sendMessage() {
         } else {
             let json = try! JSONSerialization.jsonObject(with: data!, options: []) as! [String: Any]
             print(json)
-            let responseText = json["choices"] as! [[String: Any]]
+            guard let responseText = json["choices"] as? [[String: Any]] else {
+                self.messages.append(Message(content: "\(json)", sender: "API"))
+                return
+            }
             let responseMessage = responseText[0]["text"] as! String
             let responseSender = "API"
             self.messages.append(Message(content: responseMessage, sender: responseSender))
         }
     })
-    
+
     dataTask.resume()
 }
 }
@@ -153,7 +157,7 @@ struct Message: Identifiable {
             hasher.combine(sender)
         return hasher.finalize()
     }
-    
+
     var content: String
     var sender: String
 }
@@ -244,27 +248,16 @@ struct StepperView_Previews: PreviewProvider {
     }
 }
 
-//this code will create a stepper that will allow you to go from 0.0 to 1.0 and the value will be displayed in the text view.
-// you can change the range from 0.0 to 1.0 to any other range that you need.
-// the stepper value will be rounded to 2 decimal places only.
-
-
 /*
-import SwiftUI
-
 struct ChatThread: View {
-    
-    @State var newMessage: String
-    
-    @State private var messages = [
-        Message(content: "Hey, how's it going?", sender: "John"),
-        Message(content: "Not too bad, how about you?", sender: "Jane"),
-        Message(content: "I'm good, thanks for asking. What's up?", sender: "John"),
-        Message(content: "Just wanted to catch up and see how things are going.", sender: "Jane"),
-        Message(content: "Things are going well, thanks for asking. How about you?", sender: "John"),
-        Message(content: "I'm good too, thanks. Let's catch up again soon.", sender: "Jane")
-    ]
-    
+    @State private var messages = [Message]()
+    @State private var newMessage = ""
+    @State private var showAlert = false
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var apiKey = "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"
+    @State private var temperature = 0.0
+
     var body: some View {
         VStack {
             List {
@@ -275,42 +268,173 @@ struct ChatThread: View {
                         Spacer()
                         Text(message.content)
                             .padding(.all, 10)
-                            .background(Color.gray)
+                            .foregroundColor(.white)
+                            .background(Color.blue)
                             .cornerRadius(10)
                     }
                 }
             }
+
             HStack {
                 TextField("Enter message", text: $newMessage)
+                    .clearButton(text: $newMessage)
+                    .minimumScaleFactor(0.3)
+                    .lineLimit(3)
+
                 Button(action: {
-                    self.messages.append(Message(content: self.newMessage, sender: "Me"))
-                    self.newMessage = ""
+                    self.sendMessage()
                 }) {
                     Text("Send")
                 }
             }
             .padding()
+            .shadow(radius: 5)
+
+            Stepper("Temperature \(temperature, specifier: "%.1f")", value: $temperature, in: 0.0...1.0, step: 0.1)
+                .padding()
+                .shadow(radius: 5)
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
         }
     }
+
+    func sendMessage() {
+        Task {
+            if newMessage == "" {
+                self.alertTitle = "Error"
+                self.alertMessage = "Please enter a message to send."
+                self.showAlert = true
+                return
+            }
+
+            let message = Message(content: newMessage, sender: "Me")
+            messages.append(message)
+            newMessage = ""
+
+            // Send message to chatgpt API
+            let parameters:[String : Any] = [
+                "prompt": message.content,
+                "temperature": temperature,
+            ]
+            let headers = ["Content-Type": "application/json", "Authorization": "Bearer \(apiKey)"]
+            let request = NSMutableURLRequest(url: NSURL(string: "https://api.openai.com/v1/engines/davinci/completions")! as URL,
+                                                    cachePolicy: .useProtocolCachePolicy,
+                                                timeoutInterval: 10.0)
+            request.httpMethod = "POST"
+            request.allHTTPHeaderFields = headers
+            request.httpBody = try! JSONSerialization.data(withJSONObject: parameters, options: [])
+
+            do {
+                let (data, response) = try await URLSession.shared.data(for: request as URLRequest)
+                if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                    let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                    print(json)
+                    guard let responseText = json["choices"] as? [[String: Any]] else {
+                        self.messages.append(Message(content: "\(json)", sender: "API"))
+                        return
+                    }
+                    let responseMessage = responseText[0]["text"] as! String
+                    let responseSender = "API"
+                    self.messages.append(Message(content: responseMessage, sender: responseSender))
+                } else {
+                    print(response)
+                    self.alertTitle = "Error"
+                    self.alertMessage = "Unexpected response from server."
+                    self.showAlert = true
+                }
+            } catch {
+                self.alertTitle = "Error"
+                self.alertMessage = error.localizedDescription
+                self.showAlert = true
+            }
+        }
+    }
+
+
 }
 
 struct Message: Identifiable {
     var id: Int {
         var hasher = Hasher()
-            hasher.combine(content)
-            hasher.combine(sender)
+        hasher.combine(content)
+        hasher.combine(sender)
         return hasher.finalize()
     }
-    
+
     var content: String
     var sender: String
 }
 
-struct ChatThread_Previews: PreviewProvider {
-    static var previews: some View {
-        ChatThread(newMessage: "hellow world")
+struct CompletionRequest: Codable {
+    let prompt: String
+    let max_tokens: Int
+    let temperature: Double
+    let n: Int
+    let stop: [String]
+    let model: String
+
+    init(prompt: String, temperature: Double, apiKey: String) {
+        self.prompt = prompt
+        self.temperature = temperature
+        self.max_tokens = 50
+        self.n = 1
+        self.stop = ["\n"]
+        self.model = "davinci"
+    }
+}
+
+struct CompletionResponse: Codable {
+    let choices: [Choice]
+}
+
+struct Choice: Codable {
+    var text: String
+    var index: Int?
+    var logprobs: Logprobs?
+    var finishReason: String?
+    var selectedTokens: [Int]?
+    var context: String?
+    var prompt: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case text
+        case index
+        case logprobs
+        case finishReason = "finish_reason"
+        case selectedTokens = "selected_tokens"
+        case context
+        case prompt
+    }
+}
+
+struct Logprobs: Codable {
+    var tokens: [[String: Double]]
+    var textOffset: [Int]
+    var tokenOffset: [Int]
+    var topLogprobs: [String: Double]
+    
+    enum CodingKeys: String, CodingKey {
+        case tokens
+        case textOffset = "text_offset"
+        case tokenOffset = "token_offset"
+        case topLogprobs = "top_logprobs"
+    }
+}
+
+// Color Extension
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgbValue: UInt64 = 0
+        if scanner.scanHexInt64(&rgbValue) {
+            let r = Double((rgbValue & 0xff0000) >> 16) / 255.0
+            let g = Double((rgbValue & 0xff00) >> 8) / 255.0
+            let b = Double(rgbValue & 0xff) / 255.0
+            self.init(red: r, green: g, blue: b)
+            return
+        }
+        self.init(red: 0, green: 0, blue: 0)
     }
 }
 */
-// -------------
-
