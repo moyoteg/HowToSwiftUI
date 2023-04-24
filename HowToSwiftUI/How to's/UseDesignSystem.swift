bf0c9e48f6ce4003370f55ea64418562556f3c38
Moi Gutierrez <moyoteg@gmail.com>
2023-04-23 22:55:51 -0500

    removed unused entitlements

---
+++ HowToSwiftUI/How to's/UseDesignSystem.swift
@@ -0,0 +1,20 @@
+//
+//  UseDesignSystem.swift
+//  HowToSwiftUI
+//
+//  Created by Moi Gutierrez on 4/23/23.
+//
+
+import SwiftUI
+
+struct UseDesignSystem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UseDesignSystem_Previews: PreviewProvider {
+    static var previews: some View {
+        UseDesignSystem()
+    }
+}
