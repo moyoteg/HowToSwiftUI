409d15bae48bab53d20443ef26c0153df0f1cc72
Moi Gutierrez <moyoteg@gmail.com>
2023-04-24 18:18:40 -0500

    added UseMachineLearning

---
+++ HowToSwiftUI/How to's/UseMachineLearning.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Moi Gutierrez on 4/24/23.
+//
+
+import SwiftUI
+
+struct UseMachineLearning: View {
+    var body: some View {
+        Text("UseMachineLearning")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        UseMachineLearning()
+    }
+}
